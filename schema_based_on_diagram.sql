0df14eded1607eb2eda1fb6e69c2f0a135a619c9
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-11-03 13:53:00 +0000

    Modify patients table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,9 +1,9 @@
-CREATE TABLE patients
-(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  date_of_birth DATE
-);
+-- CREATE TABLE patients
+-- (
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(255),
+--   date_of_birth DATE
+-- );
 
 CREATE TABLE medical_histories
 (
